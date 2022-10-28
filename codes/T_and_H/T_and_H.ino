f95e541ff427a0b03a2ffce1cab4652ca14e1c9f
thilanka wickrama <81402568+ThilankaMORA@users.noreply.github.com>
2022-10-28 13:44:37 +0530

    Add files via upload

---
+++ codes/T_and_H/T_and_H.ino
@@ -0,0 +1,36 @@
+#include <dht.h>
+
+dht T_and_H;
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+
+  int val = T_and_H.read11(7);
+  Serial.print("\n Tem = ");
+  Serial.print(T_and_H.temperature);
+  Serial.print("C");
+  Serial.print("   Humi = ");
+  Serial.print(T_and_H.humidity);
+  Serial.print("%");
+  delay(1000);
+
+  
+
+
+
+
+
+
+
+
+
+  
+
+}
---
+++ codes/sketch_aug09a/sketch_aug09a.ino
@@ -0,0 +1,47 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+int sensorPin = A0; 
+int sensorValue;  
+int limit = 300; 
+
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  lcd.init();                      // initialize the lcd 
+  lcd.init();
+  // Print a message to the LCD.
+  lcd.backlight();
+  //lcd.noBacklight();
+  lcd.setCursor(1,0);
+  lcd.print("Athugalpura");
+  lcd.setCursor(1,1);
+  lcd.print("konichiwaa");
+}
+
+
+void loop() {
+
+ sensorValue = analogRead(sensorPin); 
+  delay(1000); 
+ Serial.println("Analog Value : ");
+ Serial.println(sensorValue);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Anlg Val:");
+  lcd.setCursor(9,0);
+  lcd.print(sensorValue);
+ 
+ if (sensorValue<limit) {
+ digitalWrite(13, HIGH); 
+ }
+ else {
+ digitalWrite(13, LOW); 
+ }
+ 
+ delay(1000); 
+}
